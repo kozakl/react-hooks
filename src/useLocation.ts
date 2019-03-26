c3f07aa87c39ffec139e16678f23a390ff11b761
Luke Kozak <kozakluke@gmail.com>
2019-03-26 11:41:14 +0100

    Create useLocation hook

---
+++ src/useLocation.ts
@@ -0,0 +1,18 @@
+import {useEffect, useState} from 'react';
+
+export function useLocation()
+{
+    const [location, setLocation] = useState(window.location);
+    
+    useEffect(()=> {
+        window.addEventListener('hashchange', onChange);
+        
+        function onChange() {
+            setLocation(window.location);
+        }
+        return ()=>
+            window.removeEventListener('hashchange', onChange);
+    }, []);
+    
+    return location;
+}
