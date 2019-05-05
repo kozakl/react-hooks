614d0e2bf150f725a047a70fec68ca4f2b48c059
Luke Kozak <kozakluke@gmail.com>
2019-05-05 08:58:16 +0200

    Add initCall arg to useResize

--- src/useResize.ts
+++ src/useResize.ts
@@ -1,10 +1,13 @@
 import {useEffect} from 'react';
 
-export function useResize(resize:()=> void)
+export function useResize(resize:()=> void, initCall?:boolean)
 {
     useEffect(()=> {
         window.addEventListener('resize', resize);
         
+        if (initCall) {
+            resize();
+        }
         return ()=>
             window.removeEventListener('resize', resize);
     }, []);
