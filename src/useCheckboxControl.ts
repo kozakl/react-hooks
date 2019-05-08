cb0dde6c5484e17bb6b8ff5f083eb67ef287468f
Luke Kozak <kozakluke@gmail.com>
2019-05-08 13:40:25 +0200

    Mark useCheckboxControl as deprecated

--- src/useCheckboxControl.ts
+++ src/useCheckboxControl.ts
@@ -2,6 +2,7 @@ import {useState} from 'react';
 
 export function useCheckboxControl(initialChecked:boolean)
 {
+    console.log('useCheckboxControl hook is deprecated use useCheckbox hook instead');
     const [checked, setChecked] = useState(initialChecked),
           [error, setError] = useState();
     return {
