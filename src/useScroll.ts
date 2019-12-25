3b7c3eeca844d150130e66531b955b31da3a8d19
Luke Kozak <kozakluke@gmail.com>
2019-12-25 22:08:39 +0100

    Add dir to useScroll

--- src/useScroll.ts
+++ src/useScroll.ts
@@ -1,18 +1,21 @@
 import {useEffect} from 'react';
 
-export function useScroll(scroll:(value:number)=> void)
+export function useScroll(scroll:(value:number, dir?:number)=> void)
 {
     useEffect(()=> {
-        let request = true;
+        let requestAnimation = true,
+            lastScrollY = window.scrollY;
         window.addEventListener('scroll', onScroll);
         
         onScroll();
         function onScroll() {
-            if (request) {
-                request = false;
+            if (requestAnimation) {
+                requestAnimation = false;
                 window.requestAnimationFrame(()=> {
-                    request = true;
-                    scroll(window.scrollY);
+                    requestAnimation = true;
+                    
+                    scroll(window.scrollY, window.scrollY > lastScrollY ? 1 : -1);
+                    lastScrollY = window.scrollY;
                 });
             }
         }
